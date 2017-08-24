4dc86dbd5b613196fe205225ff0a4a692d8a3fc8
林龙辉 <540886643@qq.com>
2017-08-24 10:02:56 +0800

    * init;

---
+++ backpack.config.js
@@ -0,0 +1,19 @@
+
+const path = require('path')
+const resolve = path.resolve
+
+module.exports = {
+  webpack: (config, options, webpack) => {
+    // eslint
+    const eslintConfig = {
+      test: /\.(js|jsx)$/,
+      enforce: 'pre',
+      loader: 'eslint-loader',
+      exclude: [/node_modules/, resolve(__dirname, 'build')],
+    }
+    config.module.rules.unshift(eslintConfig)
+    // entry
+    config.entry.main = resolve(__dirname, 'index.js')
+    return config
+  }
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+
+import Koa from 'koa'
+
+const app = new Koa()
+
+app.use(ctx => {
+  ctx.body = 'hello koa'
+})
+
+app.listen(3000)
